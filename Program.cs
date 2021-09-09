a65ff80c0912d69611a48a29f8c547b06b727d43
unknown <karimkimo9555@gmail.com>
2021-09-09 12:15:19 +0200

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace skinet2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
